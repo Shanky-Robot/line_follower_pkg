9e5360518882dd1a4ceeda18857aa6331eedd724
Shanky B <shankyrobot@gmail.com>
2022-01-09 00:10:07 +0530

    Updated cvbridgefor ros noetic

--- src/follower_script.py
+++ src/follower_script.py
@@ -5,12 +5,14 @@
 #in the gazebo world. 
 #Please note; not happting with the whole shutdown process, in particular the use of 'gloal ctrl_c'
 
+import rospy
 import roslib
 import sys
-import rospy
 import cv2
+import cv_bridge
 import numpy
-from sensor_msgs.msg import CompressedImage
+import numpy
+from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -18,83 +20,83 @@ ctrl_c = False
 
 class LineFollower:
 
-        def __init__(self):
+    def camera_callback(self, data):
 
-                self.bridge_object = CvBridge()
-                #Subscribe to image topic
-                self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",Image, self.camera_callback)
-                #publish to /cmd_vel topic
-                self.cmd_vel_pub = rospy.Publisher("/cmd_vel",Twist, queue_size=1)
-                self.twist_object = Twist()
+        try:
+                cv_image = self.bridge_object.compressed_imgmsg_to_cv2(data, desired_encoding="passthrough")
+        except CvBridgeError as e:
+                print(e)
 
-        def clean_up(self):
-                #this function runs upon shutdown.
-                cv2.destroyAllWindows()
-                rospy.loginfo("Unregistering Camera Subscriber")
-                self.image_sub.unregister()
-                rospy.loginfo("Wait 1 second")
-                rospy.sleep(1)
-                rospy.loginfo("Stopping Motor")
-                self.twist_object.linear.x = 0.0
-                self.twist_object.angular.z = 0.0
-                self.cmd_vel_pub.publish(self.twist_object)
-                rospy.loginfo("Angular turning Value Sent = "+str(self.twist_object.angular.z))
-                rospy.loginfo("Unregistering cmd_vel Publisher")
-                self.cmd_vel_pub.unregister()
-                rospy.loginfo("Wait 1 second")
-                rospy.sleep(1)               
+        #### 1.GET IMAGE INFO AND CROP ####
+        #get info about shape of captured image
+        height, width, channels = cv_image.shape
+        #print (cv_image.shape)
+        #Crop image to only see 100 rows
+        descentre = 160
+        rows_to_watch = 100
+        crop_img = cv_image[(height)/2+descentre: (height)/2+(descentre+rows_to_watch)][1:width]
+        #print (crop_img.shape)
 
-        def camera_callback(self, data):
+        #### 2.GET IMAGE INFO AND CROP ####
+        hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+        #the follow values are pure guesses based on other code
+        lower_yellow = numpy.array([ 20, 100, 100])
+        upper_yellow = numpy.array([30, 255, 250])
 
-                try:
-			cv_image = self.bridge_object.compressed_imgmsg_to_cv2(data, desired_encoding="passthrough")
-                except CvBridgeError as e:
-                        print(e)
+        #### 3.APPLY THE MASK ####
+        mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        res = cv2.bitwise_and(crop_img,crop_img,mask= mask)
 
-                #### 1.GET IMAGE INFO AND CROP ####
-                #get info about shape of captured image
-                height, width, channels = cv_image.shape
-                #print (cv_image.shape)
-                #Crop image to only see 100 rows
-                descentre = 160
-                rows_to_watch = 100
-                crop_img = cv_image[(height)/2+descentre: (height)/2+(descentre+rows_to_watch)][1:width]
-                #print (crop_img.shape)
+        #### 4.GET THE CENTROIDS, DRAW A CIRCLE ####
+        m = cv2.moments(mask, False)
+        try:
+                cx = m['m10']/m['m00']
+                cy = m['m01']/m['m00']
+        except ZeroDivisionError:
+                cx = width/2
+                cy = height/2
+        #Draw circle on image to indicate detected centroid
+        cv2.circle(res, (int(cx), int(cy)), 10,(0,0,255), -1)
 
-                #### 2.GET IMAGE INFO AND CROP ####
-                hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
-                #the follow values are pure guesses based on other code
-                lower_yellow = numpy.array([ 20, 100, 100])
-                upper_yellow = numpy.array([30, 255, 250])
+        #Open a GUI, where you can see the contents of each image
+        cv2.imshow("Original Image", cv_image)
+        cv2.imshow("Cropped Image", crop_img)
+        cv2.imshow("HSV Image", hsv)
+        cv2.imshow("Mask", res)
+        cv2.waitKey(1)
 
-                #### 3.APPLY THE MASK ####
-                mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-                res = cv2.bitwise_and(crop_img,crop_img,mask= mask)
+        #### 5. MOVE TURTLEBOT BASED ON POISTION OF CENTROID ####
+        error_x = cx - width / 2
+        self.twist_object.linear.x = 0.1
+        self.twist_object.angular.z = -error_x / 500
+        rospy.loginfo("Angular turning Value Sent = "+str(self.twist_object.angular.z))
+        self.cmd_vel_pub.publish(self.twist_object)
 
-                #### 4.GET THE CENTROIDS, DRAW A CIRCLE ####
-                m = cv2.moments(mask, False)
-                try:
-                        cx = m['m10']/m['m00']
-                        cy = m['m01']/m['m00']
-                except ZeroDivisionError:
-                        cx = width/2
-                        cy = height/2
-                #Draw circle on image to indicate detected centroid
-                cv2.circle(res, (int(cx), int(cy)), 10,(0,0,255), -1)
+    def __init__(self):
 
-                #Open a GUI, where you can see the contents of each image
-                cv2.imshow("Original Image", cv_image)
-                cv2.imshow("Cropped Image", crop_img)
-                cv2.imshow("HSV Image", hsv)
-                cv2.imshow("Mask", res)
-                cv2.waitKey(1)
+        self.bridge_object = CvBridge()
+        #Subscribe to image topic
+        self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",Image, self.camera_callback)
+        #publish to /cmd_vel topic
+        self.cmd_vel_pub = rospy.Publisher("/cmd_vel",Twist, queue_size=1)
+        self.twist_object = Twist()
 
-                #### 5. MOVE TURTLEBOT BASED ON POISTION OF CENTROID ####
-                error_x = cx - width / 2
-                self.twist_object.linear.x = 0.1
-                self.twist_object.angular.z = -error_x / 500
-                rospy.loginfo("Angular turning Value Sent = "+str(self.twist_object.angular.z))
-                self.cmd_vel_pub.publish(self.twist_object)
+def clean_up(self):
+        #this function runs upon shutdown.
+        cv2.destroyAllWindows()
+        rospy.loginfo("Unregistering Camera Subscriber")
+        self.image_sub.unregister()
+        rospy.loginfo("Wait 1 second")
+        rospy.sleep(1)
+        rospy.loginfo("Stopping Motor")
+        self.twist_object.linear.x = 0.0
+        self.twist_object.angular.z = 0.0
+        self.cmd_vel_pub.publish(self.twist_object)
+        rospy.loginfo("Angular turning Value Sent = "+str(self.twist_object.angular.z))
+        rospy.loginfo("Unregistering cmd_vel Publisher")
+        self.cmd_vel_pub.unregister()
+        rospy.loginfo("Wait 1 second")
+        rospy.sleep(1)               
 
 def main():
 
@@ -119,4 +121,3 @@ def main():
 
 if __name__ == '__main__':
         main()
-
